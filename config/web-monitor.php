876b0e2c6955e715239ca2853b887f6133c3dcfc
rs-sliske <rs-sliske@users.noreply.github.com>
2018-10-30 15:59:12 +0000

    default url

--- config/web-monitor.php
+++ config/web-monitor.php
@@ -2,7 +2,7 @@
 
 return [
     'enabled' => env('WEB_MONITOR_ENABLED', false),
-    'base_url' => env('WEB_MONITOR_BASE_URL'),
+    'base_url' => env('WEB_MONITOR_BASE_URL', 'https://netmonitoring.co.uk'),
     'client_id' => env('WEB_MONITOR_CLIENT_ID'),
     'client_secret' => env('WEB_MONITOR_CLIENT_SECRET'),
     'queue' => env('WEB_MONITOR_QUEUE', 'default'),
